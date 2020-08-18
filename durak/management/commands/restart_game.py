f7820bfc24d16614362d02a31e197d23b4f36f1c
Maxim Pertsov <maxim.pertsov@gmail.com>
2020-08-18 01:18:03 -0400

    set low rank in restart command

--- durak/management/commands/restart_game.py
+++ durak/management/commands/restart_game.py
@@ -1,9 +1,9 @@
-from django.core.management.base import BaseCommand
-
-from durak.models import Game
-from durak.models import Card, DrawCard
-from django.utils.functional import cached_property
 from random import shuffle
+
+from django.core.management.base import BaseCommand
+from django.utils.functional import cached_property
+
+from durak.models import Card, DrawCard, Game
 
 
 class Command(BaseCommand):
@@ -11,16 +11,28 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument("--slug", type=str, help="Game slug", required=True)
+        parser.add_argument(
+            "--low-six", action="store_true", help="Lowest rank is a six",
+        )
 
-    def handle(self, slug=None, *args, **kwargs):
+    def handle(self, slug=None, low_six=None, *args, **kwargs):
         self.slug = slug
+        self.low_six = low_six
 
         try:
             self.restart_game()
+            self.remove_ranks_below_six()
             self.write_success(f"Reset Game {self.game}")
         except Game.DoesNotExist as e:
             self.stdout.write(e, self.style.ERROR)
             return
+
+    def remove_ranks_below_six(self):
+        if not self.low_six:
+            return
+
+        DrawCard.objects.filter(game__slug=self.slug, card__rank__in="2345").delete()
+        self.write_success(f"Removed all ranks below six in {self.game}")
 
     def restart_game(self):
         self.delete_events()
@@ -28,7 +40,7 @@ class Command(BaseCommand):
 
     def shuffle_cards(self):
         self.game.draw_pile.all().delete()
-        card_ids = list(Card.objects.values_list('pk', flat=True))
+        card_ids = list(Card.objects.values_list("pk", flat=True))
         shuffle(card_ids)
         for sort_key, card_id in enumerate(card_ids):
             DrawCard.objects.create(game=self.game, card_id=card_id, sort_key=sort_key)
